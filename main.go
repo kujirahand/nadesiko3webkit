9837531835979ff7c7187045f988289f808cdeb4
kujirahand_colab <web@kujirahand.com>
2021-10-17 14:09:45 +0900

    index.jsonを読んでWindowのタイトルやサイズを指定できるようにした

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
-	//webview "github.com/jchv/go-webview2"
+	"strconv"
+	"time"
 )
 
 const LOCAL_SERVER_ADDR = "127.0.0.1:17145"
+
+type IndexInfo struct {
+	Title  string `json:"title"`
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
+}
 
 func Exists(name string) bool {
 	_, err := os.Stat(name)
@@ -44,4 +54,21 @@ func indexErrorHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(404)
 	w.Header().Set("Content-Type", "text/html; charset=utf8")
 	w.Write([]byte("<html><body><h1>Loading Error</h1></body></html>"))
+}
+
+func ReadIndexJson() IndexInfo {
+	// index.json
+	var info IndexInfo = IndexInfo{Title: "なでしこ3", Width: 640, Height: 400}
+	indexJson := filepath.Join(GetBokanPath(), "webapp", "index.json")
+	raw, err := ioutil.ReadFile(indexJson)
+	if err == nil { // 読み込めた時
+		json.Unmarshal(raw, &info)
+		fmt.Printf("size=%d,%d\n", info.Width, info.Height)
+	}
+	return info
+}
+
+func GetIndexURI() string {
+	utime := strconv.FormatInt(time.Now().Unix(), 16)
+	return "http://" + LOCAL_SERVER_ADDR + "/webapp/index.html?time=" + utime
 }
--- main_mac.go
+++ main_mac.go
@@ -4,21 +4,18 @@
 package main
 
 import (
-	"strconv"
-	"time"
-
 	"github.com/webview/webview"
 )
 
 func main() {
 	debug := true
-	utime := strconv.FormatInt(time.Now().Unix(), 16)
 
 	go StartServer()
 	w := webview.New(debug)
 	defer w.Destroy()
-	w.SetTitle("nadesiko3")
-	w.SetSize(800, 600, webview.HintNone)
-	w.Navigate("http://" + LOCAL_SERVER_ADDR + "/webapp/index.html?time=" + utime)
+	info := ReadIndexJson()
+	w.SetTitle(info.Title)
+	w.SetSize(info.Width, info.Height, webview.HintNone)
+	w.Navigate(GetIndexURI())
 	w.Run()
 }
