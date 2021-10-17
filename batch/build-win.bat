21ac7302daf44a205c0233f34543171bb76e2168
kujirahand <web@kujirahand.com>
2021-10-17 21:32:07 +0900

    update batch file

--- batch/build-win.bat
+++ batch/build-win.bat
@@ -4,12 +4,12 @@ set SCRIPT_DIR=%~dp0
 cd %SCRIPT_DIR%
 cd ..
 set ROOT_DIR=%CD%\
-set TARGET_DIR=%SCRIPT_DIR%win32
+set TARGET_DIR=%SCRIPT_DIR%win-nako3
 
 rem --- build ---
 copy %SCRIPT_DIR%\res\nadesiko3webkit.syso %ROOT_DIR%nadesiko3webkit.syso
 go build -ldflags="-H windowsgui" -o %TARGET_DIR%\nadesiko3.exe
 xcopy %ROOT_DIR%webapp\* %TARGET_DIR%\webapp\ /s /d /e /h /r /y
-
-rem --- icon ---
+copy %SCRIPT_DIR%\res\README.md %TARGET_DIR%\
+echo "ok"
 
