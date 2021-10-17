bdc14be31b2d9f8d220f70eefd239b83ae3db7dc
kujirahand_colab <web@kujirahand.com>
2021-10-17 21:18:25 +0900

    macで問題なくビルドできるように修正

--- batch/build-mac.sh
+++ batch/build-mac.sh
@@ -3,17 +3,23 @@
 APP_NAME=nadesiko3
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-TARGET_PATH=$SCRIPT_DIR/$APP_NAME.app
+MACOS_PATH=$SCRIPT_DIR/macos
+TARGET_PATH=$MACOS_PATH/$APP_NAME.app
 TEMPLATE_PATH=$SCRIPT_DIR/res/Template.app
 
 cd $SCRIPT_DIR
 cd ..
-rm -f -r $TARGET_PATH
-cp -r $TEMPLATE_PATH $TARGET_PATH
-mkdir -p $TARGET_PATH/Contents/MacOS
-mkdir -p $TARGET_PATH/Contents/Resources
+rm -f -r $MACOS_PATH
 
+echo "COPY TEMPLATE"
+mkdir -p $MACOS_PATH
+cp -r $TEMPLATE_PATH $TARGET_PATH
+mkdir -p $TARGET_PATH/Contents/Resources
+cp $SCRIPT_DIR/res/README.md $MACOS_PATH/
+
+echo "BUILD"
 go build -o $TARGET_PATH/Contents/MacOS/$APP_NAME
+echo "COPY RESOURCES"
 cp -r ./webapp $TARGET_PATH/Contents/MacOS/
 
 
---
+++ batch/res/README.md
@@ -0,0 +1,13 @@
+# 配布用 なでしこ3 (webkit) バイナリ
+
+このアーカイブは、日本語プログラミング言語「なでしこ3」で作ったプログラムを実行ファイルとしてユーザーに配布したい場合に便利なツールです。
+
+Windows版とmacOS版のバイナリファイルが提供されています。
+
+ - [配布URL] https://github.com/kujirahand/nadesiko3webkit/releases
+
+# プログラムの配置方法
+
+上記の配布URLから、win-nako3.zip / mac-nako3.zip をダウンロードします。
+ZIPファイルを解凍したら、nadesiko3の実行ファイルをダブルクリックしてみてください。
+無事に
