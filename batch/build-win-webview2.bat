3ed12c2c30503626876ae8e50167d451e919f282
kujirahand <web@kujirahand.com>
2021-11-01 23:49:24 +0900

    update batch files

--- batch/build-win-webview2.bat
+++ batch/build-win-webview2.bat
@@ -1,23 +1,30 @@
-rem nadesiko3
+@echo off
+@rem nadesiko3
 
 set SCRIPT_DIR=%~dp0
 cd %SCRIPT_DIR%
 cd ..
 set ROOT_DIR=%CD%\
-set TARGET_DIR=%SCRIPT_DIR%win-nako3-webview2
+set TARGET_DIR=%SCRIPT_DIR%win-nako3-webview2\
+
+echo --------------------------------------------------------------
+echo ROOT_DIR=%ROOT_DIR%
+echo SCRIPT_DIR=%SCRIPT_DIR%
+echo TARGET_DIR=%TARGET_DIR%
+echo --------------------------------------------------------------
 
 rem --- prepare webview2 ---
-%ROOT_DIR%make_webview_win.bat
+copy %ROOT_DIR%_view_webview_win.go %ROOT_DIR%view.go
 
 rem --- reset ---
 rmdir /s /q %TARGET_DIR%
 mkdir %TARGET_DIR%
 
 rem --- build ---
-copy %SCRIPT_DIR%\res\nadesiko3webkit.syso %ROOT_DIR%nadesiko3webkit.syso
-go build -ldflags="-H windowsgui" -o %TARGET_DIR%\nadesiko3.exe
-xcopy %ROOT_DIR%webapp\* %TARGET_DIR%\webapp\ /s /d /e /h /r /y
-copy %SCRIPT_DIR%\res\README.md %TARGET_DIR%\
+copy %SCRIPT_DIR%res\nadesiko3webkit.syso %ROOT_DIR%nadesiko3webkit.syso
+go build -ldflags="-H windowsgui" -o %TARGET_DIR%nadesiko3.exe
+xcopy %ROOT_DIR%webapp\* %TARGET_DIR%webapp\ /s /d /e /h /r /y
+copy %SCRIPT_DIR%res\README.md %TARGET_DIR%\
 
 echo "ok"
 pause
--- make_webview_win.bat
+++ make_webview_win.bat
@@ -1,3 +1,4 @@
+rem copy view
 copy _view_webview_win.go view.go
 go run .
 
