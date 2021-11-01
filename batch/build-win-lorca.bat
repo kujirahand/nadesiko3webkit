214d0c0cd543c6552198d287174303e0badb4c5d
kujirahand_colab <web@kujirahand.com>
2021-11-01 22:41:49 +0900

    add build-win*.bat

---
+++ batch/build-win-lorca.bat
@@ -0,0 +1,23 @@
+rem nadesiko3
+
+set SCRIPT_DIR=%~dp0
+cd %SCRIPT_DIR%
+cd ..
+set ROOT_DIR=%CD%\
+set TARGET_DIR=%SCRIPT_DIR%win-nako3-chrome
+
+rem --- prepare lorca ---
+%ROOT_DIR%make_lorca.bat
+
+rem --- reset ---
+rmdir /s /q %TARGET_DIR%
+mkdir %TARGET_DIR%
+
+rem --- build ---
+copy %SCRIPT_DIR%\res\nadesiko3webkit.syso %ROOT_DIR%nadesiko3webkit.syso
+go build -ldflags="-H windowsgui" -o %TARGET_DIR%\nadesiko3.exe
+xcopy %ROOT_DIR%webapp\* %TARGET_DIR%\webapp\ /s /d /e /h /r /y
+copy %SCRIPT_DIR%\res\README.md %TARGET_DIR%\
+
+echo "ok"
+
--- batch/build-win.bat
+++ batch/build-win.bat
@@ -1,16 +1,4 @@
-rem nadesiko3
-
-set SCRIPT_DIR=%~dp0
-cd %SCRIPT_DIR%
-cd ..
-set ROOT_DIR=%CD%\
-set TARGET_DIR=%SCRIPT_DIR%win-nako3
-
-rem --- build ---
-rmdir /s /q %TARGET_DIR%
-copy %SCRIPT_DIR%\res\nadesiko3webkit.syso %ROOT_DIR%nadesiko3webkit.syso
-go build -ldflags="-H windowsgui" -o %TARGET_DIR%\nadesiko3.exe
-xcopy %ROOT_DIR%webapp\* %TARGET_DIR%\webapp\ /s /d /e /h /r /y
-copy %SCRIPT_DIR%\res\README.md %TARGET_DIR%\
-echo "ok"
-
+rem BUILD WebView2 and Lorca
+.\build-win-lorca.bat
+.\build-win-webview2.bat
+pause
